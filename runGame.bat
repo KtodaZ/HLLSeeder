438543136443ce7d169ac1249ff9b521d36c47a2
Kyle Szombathy <kyleszombathy@gmail.com>
2022-11-21 16:34:42 -0800

    Add second timeout line to fix windows 11 bug

--- runGame.bat
+++ runGame.bat
@@ -4,12 +4,13 @@ for /f "delims=" %%x in (config.txt) do (set "%%x")
 
 rem Clicks URL to open game
 START "" %serverUrl%
+timeout /t 5
 timeout /t 60
 rem Click URL again to join server, it takes 2 clicks since last patch
 START "" %serverUrl%
 
 rem IF YOU HAVE PROBLEMS CONNECTING TO THE SERVER, TRY UNCOMMENTING THE LINES BELOW BY REMOVING 'rem'
-rem SLEEP 20
+rem timeout /t 20
 rem START "" %serverUrl%
 
 ECHO "Game will automatically close in %closeGameAfterMinutes% minutes from this message"
