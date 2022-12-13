9449e72e210d07031fa4a17f960571424d1b37f6
Kyle Szombathy <KtodaZ@users.noreply.github.com>
2022-12-13 12:11:05 -0800

    add /nobreak to timeout calls

--- runGame.bat
+++ runGame.bat
@@ -4,19 +4,15 @@ for /f "delims=" %%x in (config.txt) do (set "%%x")
 
 rem Clicks URL to open game
 START "" %serverUrl%
-timeout /t 5
-timeout /t 60
+timeout /t 5 /nobreak
+timeout /t 60 /nobreak
 rem Click URL again to join server, it takes 2 clicks since last patch
 START "" %serverUrl%
-
-rem IF YOU HAVE PROBLEMS CONNECTING TO THE SERVER, TRY UNCOMMENTING THE LINES BELOW BY REMOVING 'rem'
-rem timeout /t 20
-rem START "" %serverUrl%
 
 ECHO "Game will automatically close in %closeGameAfterMinutes% minutes from this message"
 ECHO "Close this window to cancel the process"
 set /A timeout =  %closeGameAfterMinutes%*60
-timeout /t %timeout%
+timeout /t %timeout% /nobreak
 
 rem Kill HLL
 TASKKILL /IM HLL-Win64-Shipping.exe /F
